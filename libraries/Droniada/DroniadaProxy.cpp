0b5a605f61a4a618147f11ee298af50d4c084b76
bladekp <bladekp@ee.pw.edu.pl>
2017-05-25 16:41:08 +0200

    transreceiver simulator

--- libraries/Droniada/DroniadaProxy.cpp
+++ libraries/Droniada/DroniadaProxy.cpp
@@ -5,6 +5,17 @@
 
 extern const AP_HAL::HAL& hal;
 
+typedef struct droniada 
+{
+    unsigned char major; //1B
+    unsigned char minor; //1B
+    unsigned short int rssi; //2B
+    unsigned long int crc; //8B
+} Droniada;
+
+unsigned char buff[12];
+int data_length = 0;
+
 void DroniadaProxy::init()
 {
     if (hal.uartE == NULL) {
@@ -23,7 +34,24 @@ void DroniadaProxy::update()
     int16_t nbytes = hal.uartE->available();
     while (nbytes-- > 0) {
         char c = hal.uartE->read();
-    	hal.uartE->printf("%.3f second char: %c\n", AP_HAL::millis()*0.001f, c);
+    	if (data_length < 2){
+		if (c == 0x33){ //TODO: change to 0xAA
+			data_length++;
+		} else {
+			data_length = 0;
+		}
+		break;	
+	}
+	buff[data_length++ - 2] = c;
+	if(data_length == 14){
+		data_length = 0;
+		Droniada d;
+		d.major = buff[0];
+		d.minor = buff[1];
+		d.rssi = (buff[3] << 8) + buff[2];
+		//TODO d.crc = ...
+		hal.uartE->printf("Major: %c Minor: %c, Rssi: %d,", d.major, d.minor, d.rssi);
+	}
     }
     
 }
