13f0aa1877c44a7447da0ebdf33691030a76abeb
bladekp <bladekp@ee.pw.edu.pl>
2017-05-25 15:07:35 +0200

    uart test

--- libraries/Droniada/DroniadaProxy.cpp
+++ libraries/Droniada/DroniadaProxy.cpp
@@ -1,12 +1,20 @@
 #include <AP_HAL/AP_HAL.h>
 #include "DroniadaProxy.h"
 
+extern const AP_HAL::HAL& hal;
+
 void DroniadaProxy::init()
 {
-
+    if (hal.uartE == NULL) {
+        return;
+    }
+    hal.uartE->begin(57600);
 }
 
 void DroniadaProxy::update()
 {
-
+    if (hal.uartE == NULL) {
+        return;
+    }
+    hal.uartE->printf("Hello on uartE at %.3f seconds\n", AP_HAL::millis()*0.001f);
 }
