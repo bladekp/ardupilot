6560a20affd2d1739072526808127666146be261
bladekp <bladekp@ee.pw.edu.pl>
2017-05-25 15:29:25 +0200

    echo test

--- libraries/Droniada/DroniadaProxy.cpp
+++ libraries/Droniada/DroniadaProxy.cpp
@@ -1,6 +1,8 @@
 #include <AP_HAL/AP_HAL.h>
 #include "DroniadaProxy.h"
 
+//TODO: better to use AP_SerialManager here, like in ardupilot/libraries/AP_RangeFinder/AP_RangeFinder_LightWareSerial.cpp for example
+
 extern const AP_HAL::HAL& hal;
 
 void DroniadaProxy::init()
@@ -11,10 +13,17 @@ void DroniadaProxy::init()
     hal.uartE->begin(57600);
 }
 
+//currently 10Hz loop
 void DroniadaProxy::update()
 {
     if (hal.uartE == NULL) {
         return;
     }
-    hal.uartE->printf("Hello on uartE at %.3f seconds\n", AP_HAL::millis()*0.001f);
+    
+    int16_t nbytes = hal.uartE->available();
+    while (nbytes-- > 0) {
+        char c = hal.uartE->read();
+    	hal.uartE->printf("%.3f second char: %c\n", AP_HAL::millis()*0.001f, c);
+    }
+    
 }
